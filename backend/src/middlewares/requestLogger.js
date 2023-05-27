5a92982699799b4457500cac45de3457ba99103b
Luca Favini <luca.favini.02@gmail.com>
2023-05-27 22:09:48 +0200

    fix(middlewares): add `headers` to `requestLogger`

--- backend/src/middlewares/requestLogger.js
+++ backend/src/middlewares/requestLogger.js
@@ -2,22 +2,11 @@ import logger from "../utils/logger.js"
 
 // logs every request to the api
 const requestLogger = (req, res, next) => {
+  logger.info("---")
   logger.info("Method:", req.method)
   logger.info("Path:  ", req.path)
-
-  // sanitize request body (password, tokens)
-  var sanitizedBody = structuredClone(req.body)
-
-  if (sanitizedBody.password) {
-    sanitizedBody.password = "****"
-  }
-
-  if (sanitizedBody.token) {
-    sanitizedBody.token = "****"
-  }
-
-  logger.info("Body:  ", sanitizedBody)
-
+  logger.info("Body:  ", req.body)
+  logger.info("Headers:  ", req.headers)
   logger.info("---")
   next()
 }
