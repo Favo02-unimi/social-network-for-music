cbcb0f9a3b27d7245ecbbe89ab401e9a32524823
Luca Favini <luca.favini.02@gmail.com>
2023-05-28 14:47:48 +0200

    docs(utils): add `JSdoc` to `spotifyToken` util

--- backend/src/utils/spotifyToken.js
+++ backend/src/utils/spotifyToken.js
@@ -2,6 +2,11 @@ import config from "./config.js"
 
 const URL = "https://accounts.spotify.com/api/token"
 
+/**
+ * Request a Bearer token to Spotify API, returning a promise
+ * @requires .env CLIENT_ID and CLIENT_SECRET
+ * @returns {Promise}
+ */
 const fetchToken = async () => {
 
   const basicAuth = new Buffer.from(`${config.CLIENT_ID}:${config.CLIENT_SECRET}`).toString("base64")
