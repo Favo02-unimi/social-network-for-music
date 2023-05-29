7f4f06afa048aa31d90d58ef43e2931cfd5faced
Luca Favini <luca.favini.02@gmail.com>
2023-05-29 13:04:42 +0200

    fix(controllers): add fields validation in `users` controller

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt"
 import express from "express"
 import authenticateUser from "../middlewares/authenticateUser.js"
+import REGEX from "../utils/regex.js"
 import User from "../models/User.js"
 
 const usersRouter = express.Router()
@@ -31,7 +32,30 @@ usersRouter.post("/create", async (req, res) => {
     favouriteGenres
   } = req.body
 
-  // TODO: fields validation
+  // fields validation
+  if (!username || !REGEX.username.test(username)) {
+    return res.status(400).json({ error: `Enter a valid username: ${REGEX.usernameDesc}` })
+  }
+  if (!password || !REGEX.password.test(password)) {
+    return res.status(400).json({ error: `Enter a valid password: ${REGEX.passwordDesc}` })
+  }
+  if (!email || !REGEX.email.test(email)) {
+    return res.status(400).json({ error: `Enter a valid email: ${REGEX.emailDesc}` })
+  }
+
+  // unique username and email check
+  const existingUsername = await User.findOne({ username: new RegExp(`^${username}$`, "i") })
+  if (existingUsername) {
+    return res.status(400).json({ error: "Username already taken" })
+  }
+  const existingEmail = await User.findOne({ email: new RegExp(`^${email}$`, "i") })
+  if (existingEmail) {
+    return res.status(400).json({ error: "Email already used" })
+  }
+
+  // no need to validate favouriteArtists and favouriteGenres:
+  //   can be null and undefined (default will be assigned)
+  //   can be any type (automatically converted to string array)
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
