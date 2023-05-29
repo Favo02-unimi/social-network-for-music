449d6096990de29669716a9e7ba1d3068139e284
Luca Favini <luca.favini.02@gmail.com>
2023-05-29 15:12:52 +0200

    feat(controllers): add `users/edit` `patch` endpoint

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -72,4 +72,85 @@ usersRouter.post("/create", async (req, res) => {
   res.status(201).json(savedUser)
 })
 
+/**
+ * Edit current user (logged user)
+ * @param {User} body fields of the user to edit
+ * @requires authorization header (JWT token)
+ * @requires oldPassword in body to successfully update user
+ * @returns {Response}
+ */
+usersRouter.patch("/edit", authenticateUser, async (req, res) => {
+
+  const user = await User.findById(req.user.id)
+
+  // old password check
+  if (!req.body?.oldPassword) {
+    return res.status(400).json({ error: "Missing old password" })
+  }
+  const passwordCorrect = await bcrypt.compare(req.body.oldPassword, user.passwordHash)
+  if (!passwordCorrect) {
+    return res.status(401).json({ error: "Invalid old password" })
+  }
+
+  // username
+  if (req.body?.username) {
+    const username = req.body.username
+
+    if (!REGEX.username.test(username)) {
+      return res.status(400).json({ error: `Enter a valid username: ${REGEX.usernameDesc}` })
+    }
+
+    const existingUsername = await User.findOne({ username: new RegExp(`^${username}$`, "i") })
+    if (existingUsername) {
+      return res.status(400).json({ error: "Username already taken" })
+    }
+
+    user.username = username
+  }
+
+  // password
+  if (req.body?.newPassword) {
+    const newPassword = req.body.newPassword
+
+    if (!REGEX.password.test(newPassword)) {
+      return res.status(400).json({ error: `Enter a valid new password: ${REGEX.passwordDesc}` })
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(newPassword, saltRounds)
+
+    user.passwordHash = passwordHash
+  }
+
+  // email
+  if (req.body?.email) {
+    const email = req.body.email
+
+    if (!REGEX.email.test(email)) {
+      return res.status(400).json({ error: `Enter a valid email: ${REGEX.emailDesc}` })
+    }
+
+    const existingEmail = await User.findOne({ email: new RegExp(`^${email}$`, "i") })
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email already used" })
+    }
+
+    user.email = email
+  }
+
+  // favourites artists
+  if (req.body?.favouriteArtists) {
+    user.favouriteArtists = req.body.favouriteArtists
+  }
+
+  // favourites genres
+  if (req.body?.favouriteGenres) {
+    user.favouriteGenres = req.body.favouriteGenres
+  }
+
+  const updatedUser = await user.save()
+  res.status(200).json(updatedUser)
+})
+
+
 export default usersRouter
