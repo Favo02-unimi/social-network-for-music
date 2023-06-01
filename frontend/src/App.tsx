f336f69bb9476adc6a60c16ff3b0e1edc727e1c3
Luca Favini <luca.favini.02@gmail.com>
2023-06-01 17:51:32 +0200

    feat(pages): add `BrowserRouter` in `App`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,34 @@
+import type { FC } from "react"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+
+import ProtectedRoute from "./utils/ProtectedRoute"
+
 import "../src/assets/styles/index.css"
 
-function App() {
-  return (
-    <div>Social Network for Music</div>
-  )
-}
+const App : FC = () => (
+  <BrowserRouter>
+    <Routes>
+
+      <Route path="*" element={
+        <Navigate to="/home" />
+      } />
+
+      <Route path="/home" element={
+        <>Home (no login required)</>
+      } />
+
+      <Route path="/login" element={
+        <>Login</>
+      } />
+
+      <Route path="/afterlogin" element={
+        <ProtectedRoute>
+          <>Login successfull</>
+        </ProtectedRoute>
+      } />
+
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App
