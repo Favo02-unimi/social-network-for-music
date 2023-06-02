aa655417c4d702cb844638752a1afca1d6ff3c70
Luca Favini <luca.favini.02@gmail.com>
2023-06-02 17:26:36 +0200

    feat(pages): add `Login` page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,38 @@
 import type { FC } from "react"
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
+import Login from "./pages/Login"
 import ProtectedRoute from "./utils/ProtectedRoute"
 
 import "../src/assets/styles/index.css"
 
+
 const App : FC = () => (
-  <BrowserRouter>
-    <Routes>
+  <div className="h-screen w-screen bg-spotify-black text-white">
+    <BrowserRouter>
+      <Routes>
 
-      <Route path="*" element={
-        <Navigate to="/home" />
-      } />
+        <Route path="*" element={
+          <Navigate to="/home" replace />
+        } />
 
-      <Route path="/home" element={
-        <>Home (no login required)</>
-      } />
+        <Route path="/home" element={
+          <>Home (no login required)</>
+        } />
 
-      <Route path="/login" element={
-        <>Login</>
-      } />
+        <Route path="/login" element={
+          <Login />
+        } />
 
-      <Route path="/afterlogin" element={
-        <ProtectedRoute>
-          <>Login successfull</>
-        </ProtectedRoute>
-      } />
+        <Route path="/afterlogin" element={
+          <ProtectedRoute>
+            <>Login successfull</>
+          </ProtectedRoute>
+        } />
 
-    </Routes>
-  </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
+  </div>
 )
 
 export default App
