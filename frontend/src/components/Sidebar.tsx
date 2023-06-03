decef1791a3b19a490ef51ab149d9fa27cef14ce
Luca Favini <luca.favini.02@gmail.com>
2023-06-04 00:21:29 +0200

    fix(css): change `Sidebar` styles

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -34,16 +34,16 @@ const Sidebar : FC<{sideContent : ReactElement, content : ReactElement}> = ({ si
     <div className="h-screen flex flex-row">
       <div
         ref={sidebarRef}
-        className="grow-0 shrink-0 min-w-[150px] max-w-[600px] flex flex-row border-r-2 border-gray-300 shadow-md"
+        className="grow-0 shrink-0 min-w-[150px] max-w-[600px] flex flex-row shadow-md"
         style={{ width: sidebarWidth }}
         onMouseDown={(e) => e.preventDefault()}
       >
-        <div className="grow shrink">
+        <div className="grow shrink p-4">
           {sideContent}
         </div>
-        <div className="grow-0 shrink-0 basis-1 justify-self-end cursor-col-resize resize-x hover:w-1 bg-gray-300" onMouseDown={startResizing} />
+        <div className="grow-0 shrink-0 basis-1 justify-self-end cursor-col-resize resize-x hover:w-1 bg-spotify-greendark rounded my-4" onMouseDown={startResizing} />
       </div>
-      <div className="grow shrink flex flex-col h-screen shadow-md">
+      <div className="grow shrink h-screen p-4">
         {content}
       </div>
     </div>
