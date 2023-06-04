fab7c57bee5a17caaaf96243383bda61fe3a8f29
Luca Favini <luca.favini.02@gmail.com>
2023-06-04 16:35:24 +0200

    refactor(pages): use `createBrowserRouter` for routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import type { FC } from "react"
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
 
-import Loading from "./components/Loading"
+import ProtectedRoute from "./components/ProtectedRoute"
 import Sidebar from "./components/Sidebar"
-import useAuth from "./hooks/useAuth"
+import SideContent from "./components/SideContent"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 
@@ -11,67 +11,31 @@ import "../src/assets/styles/index.css"
 
 const App : FC = () => {
 
-  const [auth, isLoading] = useAuth()
+  const router = createBrowserRouter([
+    {
+      path: "*",
+      element: <Navigate to="/home" replace />
+    },
+    {
+      path: "/home",
+      element: <Sidebar sideContent={<SideContent />}><Home /></Sidebar>
+    },
+    {
+      path: "/login",
+      element: <Login />
+    },
+    {
+      path: "/",
+      element: <ProtectedRoute><Sidebar sideContent={<SideContent />} /></ProtectedRoute>,
+      children: [
+        { path: "afterlogin", element: <>Afterlogin</> }
+      ]
+    }
+  ])
 
-  if (isLoading) {
-    return (
-      <div className="h-screen w-screen bg-spotify-black text-white">
-        <Loading />
-      </div>
-    )
-  }
-
-  // no login routes
-  if (!auth) {
-    return (
-      <div className="h-screen w-screen bg-spotify-black text-white">
-        <BrowserRouter>
-          <Routes>
-
-            <Route path="/" element={
-              <Navigate to="/home" replace />
-            } />
-
-            <Route path="/home" element={
-              <Sidebar
-                sideContent={<>side</>}
-                content={<Home />}
-              />
-            } />
-
-            <Route path="/login" element={
-              <Login />
-            } />
-
-            <Route path="*" element={
-              <Navigate to="/login" replace />
-            } />
-
-          </Routes>
-        </BrowserRouter>
-      </div>
-    )
-  }
-
-  // logged in routes
   return (
     <div className="h-screen w-screen bg-spotify-black text-white">
-      <Sidebar
-        sideContent={<>side</>}
-        content={
-          <BrowserRouter>
-            <Routes>
-
-              <Route path="/home" element={<Home />} />
-
-              <Route path="/afterlogin" element={<>Login successfull</>} />
-
-              <Route path="*" element={<Navigate to="/home" replace />} />
-
-            </Routes>
-          </BrowserRouter>
-        }
-      />
+      <RouterProvider router={router} />
     </div>
   )
 }
