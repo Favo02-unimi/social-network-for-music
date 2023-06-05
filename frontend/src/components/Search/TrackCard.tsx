60e95e57658e248e0bae62c6b9ffcad586fd2859
Luca Favini <luca.favini.02@gmail.com>
2023-06-05 22:24:00 +0200

    feat(components): add `TrackCard`, `FakeTrackCard`

---
+++ frontend/src/components/Search/TrackCard.tsx
@@ -0,0 +1,23 @@
+import type { FC } from "react"
+
+import type Track from "../../interfaces/Track"
+
+const TrackCard : FC<{track : Track}> = ({ track }) => (
+  <div className="m-4 w-40 pb-2 h-60 flex flex-col items-center border border-white/20 rounded-md text-center">
+
+    <img src={track.album.images[0].url} className="w-40 h-40 rounded-t-md opacity-80" />
+
+    <h1 className="w-full my-1 px-2 font-bold text-ellipsis whitespace-nowrap overflow-hidden">{track.name}</h1>
+
+    <div className="w-full overflow-hidden">
+      {track.artists.map((a, i) =>
+        <h2 className="w-full uppercase text-xs px-2 text-ellipsis whitespace-nowrap overflow-hidden" key={a.id}>
+          {a.name}{i < track.artists.length - 1 ? ", " : ""}
+        </h2>
+      )}
+    </div>
+  </div>
+
+)
+
+export default TrackCard
