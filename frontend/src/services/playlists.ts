8823709d4052fd6cbebd7b6e39dc1ea3634e180a
Luca Favini <luca.favini.02@gmail.com>
2023-06-06 21:39:00 +0200

    feat(services): add `playlists` service
    `getPublic`, `get`, `create` available

---
+++ frontend/src/services/playlists.ts
@@ -0,0 +1,48 @@
+import axios from "axios"
+
+const baseUrl = "/api/playlists"
+
+const getPublic = async () => {
+
+  const res = await axios.get(`${baseUrl}/public`)
+
+  return res.data
+}
+
+const get = async (id : string) => {
+
+  const headers = { headers: { "authorization": localStorage.getItem("token") } }
+
+  const res = await axios.get(`${baseUrl}/${id}`, headers)
+
+  return res.data
+}
+
+const create = async (
+  title : string,
+  description : string,
+  isPublic : boolean,
+  tags ?: string[]
+) => {
+
+  const user = {
+    title,
+    description,
+    isPublic,
+    tags
+  }
+
+  const headers = { headers: { "authorization": localStorage.getItem("token") } }
+
+  const res = await axios.post(`${baseUrl}/create`, user, headers)
+
+  return res.data
+}
+
+const playlistsService = {
+  getPublic,
+  get,
+  create
+}
+
+export default playlistsService
