e3a589599e81c2363098c58a2fe5367297fe66e1
Luca Favini <luca.favini.02@gmail.com>
2023-06-15 17:05:02 +0200

    fix(components): fix `profile` link in `SideContent`
    display dynamically "login" or "profile"

--- frontend/src/components/SideContent.tsx
+++ frontend/src/components/SideContent.tsx
@@ -3,6 +3,7 @@ import { FaUser } from "react-icons/fa"
 import { Link, NavLink } from "react-router-dom"
 
 import packageInfo from "../../package.json"
+import useAuth from "../hooks/useAuth"
 
 const SideContent : FC = () => {
 
@@ -12,7 +13,7 @@ const SideContent : FC = () => {
     { to: "/playlists", text: "My Playlists" }
   ]
 
-  const username = window.localStorage.getItem("user") ?? "Login"
+  const [auth] = useAuth()
 
   return (
     <div className="w-full h-full border border-white/20 rounded-md flex flex-col justify-between text-center py-6">
@@ -38,9 +39,9 @@ const SideContent : FC = () => {
       <div>
         <div className="mb-2">
           <NavLink
-            to={username === "Login" ? "/login" : "/profile"}
-            className="inline-block px-10 py-1 bg-spotify-greendark hover:bg-spotify-green rounded-2xl tracking-widest font-medium text-lg transition-all duration-500"
-          ><FaUser className="inline text-sm -mt-1.5 mr-2" />{username}</NavLink>
+            to={auth ? "/profile" : "/login"}
+            className="uppercase inline-block px-10 py-1 bg-spotify-greendark hover:bg-spotify-green rounded-2xl tracking-widest font-medium text-lg transition-all duration-500"
+          ><FaUser className="inline text-sm -mt-1.5 mr-2" />{auth ? "profile" : "login"}</NavLink>
         </div>
 
         <div className="text-white/40 text-xs">
