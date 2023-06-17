cedba86008d84aff76792926b424c7a67ed67e6c
Luca Favini <luca.favini.02@gmail.com>
2023-06-17 02:46:50 +0200

    feat(pages): add edit favourites to `Profile` page

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -5,7 +5,8 @@ import { toast } from "react-toastify"
 
 import Loading from "../components/Loading"
 import DeleteAccount from "../components/Profile/DeleteAccount"
-import Favourites from "../components/Profile/Favourites"
+import FavouriteArtists from "../components/Profile/FavouriteArtists"
+import FavouriteGenres from "../components/Profile/FavouriteGenres"
 import type User from "../interfaces/User"
 import usersService from "../services/users"
 
@@ -48,24 +49,40 @@ const Profile : FC = () => {
     navigate("/home")
   }
 
+  if (!user) {
+    return (
+      <div className="relative w-full h-full border border-white/20 rounded-md p-6 flex flex-col justify-center items-center">
+        {isLoading && <Loading small />}
+      </div>
+    )
+  }
+
   return (
     <div className="relative w-full h-full border border-white/20 rounded-md p-6 flex flex-col justify-center items-center">
       {isLoading && <Loading small />}
 
-      <h1 className="text-3xl mb-8">Hello, <span className="text-spotify-green font-bold">{user?.username}</span></h1>
+      <h1 className="text-3xl mb-8">Hello, <span className="text-spotify-green font-bold">{user.username}</span></h1>
 
-      <Favourites title="artists" list={user?.favouriteArtists} />
+      <FavouriteArtists
+        list={user.favouriteArtists}
+        setUser={setUser}
+        setIsLoading={setIsLoading}
+      />
 
       <div className="my-8 h-[1px] w-full border-t border-white/20" />
 
-      <Favourites title="genres" list={user?.favouriteGenres} />
+      <FavouriteGenres
+        list={user.favouriteGenres}
+        setUser={setUser}
+        setIsLoading={setIsLoading}
+      />
 
       <div className="mt-8 h-[1px] w-full border-t border-white/20" />
 
       <div className="text-center mt-4">
         <h2 className="text-xl font-bold">Personal information</h2>
-        <h3 className="mt-2">Username: <span className="ml-0.5 bg-gray-300/30 px-2 py-0.5 rounded">{user?.username}</span></h3>
-        <h3 className="mt-2">Email: <span className="ml-0.5 bg-gray-300/30 px-2 py-0.5 rounded">{user?.email}</span></h3>
+        <h3 className="mt-2">Username: <span className="ml-0.5 bg-gray-300/30 px-2 py-0.5 rounded">{user.username}</span></h3>
+        <h3 className="mt-2">Email: <span className="ml-0.5 bg-gray-300/30 px-2 py-0.5 rounded">{user.email}</span></h3>
       </div>
 
       <Link to="/profile/edit" className="mt-10 bg-spotify-greendark px-3 py-1 rounded-lg hover:bg-spotify-green font-bold transition-all duration-700">Edit profile</Link>
