19b281285a34ea1d58f2337ecf315db15ff13311
Luca Favini <luca.favini.02@gmail.com>
2023-06-18 16:04:59 +0200

    feat(services): add `add/removeCollaborator` in `playlists`
    service

--- frontend/src/services/playlists.ts
+++ frontend/src/services/playlists.ts
@@ -234,6 +234,46 @@ const followers = async (id : string) => {
   return res.data
 }
 
+/**
+ * Make @param userId collaborator of @param playlistId playlist
+ * @param {string} playlistId playlist to add collaborator
+ * @param {string} userId user to make collaborator
+ * @requires authorization header (JWT token)
+ * @throws {401} missing/invalid token
+ * @throws {404} playlist not found
+ * @throws {401} current user not creator
+ * @throws {400} user to make collaborator not follower of playlist
+ * @returns {200} updated playlist
+ */
+const addCollaborator = async (playlistId : string, userId : string) => {
+
+  const headers = { headers: { "authorization": localStorage.getItem("token") } }
+
+  const res = await axios.post(`${baseUrl}/${playlistId}/addcollaborator/${userId}`, {}, headers)
+
+  return res.data
+}
+
+/**
+ * Remove @param userId collaborator from @param playlistId playlist
+ * @param {string} playlistId playlist to remove collaborator
+ * @param {string} userId user to remove collaborator
+ * @requires authorization header (JWT token)
+ * @throws {401} missing/invalid token
+ * @throws {404} playlist not found
+ * @throws {401} current user not creator
+ * @throws {400} user to remove collaborator not collaborator of playlist
+ * @returns {200} updated playlist
+ */
+const removeCollaborator = async (playlistId : string, userId : string) => {
+
+  const headers = { headers: { "authorization": localStorage.getItem("token") } }
+
+  const res = await axios.post(`${baseUrl}/${playlistId}/removecollaborator/${userId}`, {}, headers)
+
+  return res.data
+}
+
 const playlistsService = {
   getPublic,
   getSingle,
@@ -246,6 +286,8 @@ const playlistsService = {
   follow,
   unfollow,
   followers,
+  addCollaborator,
+  removeCollaborator
 }
 
 export default playlistsService
