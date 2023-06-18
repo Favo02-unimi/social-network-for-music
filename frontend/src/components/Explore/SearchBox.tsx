aa64733a88f0abc8dad0f45b930f3bdfcca6dd0e
Luca Favini <luca.favini.02@gmail.com>
2023-06-18 02:56:23 +0200

    feat(components): add filters to `SearchBox`

--- frontend/src/components/Explore/SearchBox.tsx
+++ frontend/src/components/Explore/SearchBox.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { MdExpandLess, MdExpandMore } from "react-icons/md"
 import { useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 
@@ -9,6 +10,7 @@ import checkTokenExpiration from "../../utils/checkTokenExpiration"
 import REGEX from "../../utils/regex"
 import Loading from "../Loading"
 
+import Filters from "./Filters"
 import Recommendations from "./Recommendations"
 import TrackCard from "./TrackCard"
 
@@ -18,52 +20,68 @@ const SearchBox : FC<{
   openTrack ?: Track
 }> = ({ isLoading, setIsLoading, openTrack }) => {
 
+  const [query, setQuery] = useState<string>("")
+
+  const [filtersOpen, setFiltersOpen] = useState<boolean>(false)
+  const [artist, setArtist] = useState<string>("")
+  const [genre, setGenre] = useState<string>("")
+  const [yearMin, setYearMin] = useState<number>(2008)
+  const [yearMax, setYearMax] = useState<number>(2023)
+
   const navigate = useNavigate()
 
   const [isInvalid, setIsInvalid] = useState<boolean>(false)
 
   const [tracks, setTracks] = useState<Track[]>()
 
-  const handleSearch = async (query : string) => {
-    if (query.length < 2) {
-      setIsInvalid(false)
-      setTracks([])
-      return
-    }
-
-    setIsLoading(true)
-
-    if (!REGEX.query.test(query)) {
-      setIsLoading(false)
-      setIsInvalid(true)
-      return
-    }
-    else {
-      setIsInvalid(false)
-    }
-
-    try {
-      const { valid, message } = checkTokenExpiration()
-      if (!valid) {
-        toast.error(message)
-        navigate("/login")
+  useEffect(() => {
+    const handleSearch = async () => {
+      if (query.length < 2 && artist.length < 2 && genre.length < 2) {
+        setTracks([])
         return
       }
 
-      const res = await spotifyService.tracks(query)
-      setTracks(res?.tracks?.items)
-    }
-    catch(e) {
-      if (e?.response?.data?.error) {
-        toast.error(e.response.data.error)
-      } else {
-        toast.error("Generic error, please try again")
+      setIsLoading(true)
+
+      if (
+        (query.length >= 2 && !REGEX.query.test(query)) ||
+        (artist.length >= 2 && !REGEX.query.test(artist)) ||
+        (genre.length >= 2 && !REGEX.query.test(genre))
+      ) {
+        setIsLoading(false)
+        setIsInvalid(true)
+        return
+      }
+      else {
+        setIsInvalid(false)
+      }
+
+      try {
+        const { valid, message } = checkTokenExpiration()
+        if (!valid) {
+          toast.error(message)
+          navigate("/login")
+          return
+        }
+
+        const res = await spotifyService.tracks(query, artist, genre, yearMin, yearMax)
+        setTracks(res?.tracks?.items)
+      }
+      catch(e) {
+        if (e?.response?.data?.error) {
+          toast.error(e.response.data.error)
+        } else {
+          toast.error("Generic error, please try again")
+        }
+      }
+      finally {
+        setIsLoading(false)
       }
     }
-    finally {
-      setIsLoading(false)
-    }
-  }
+
+
+    handleSearch()
+  }, [query, artist, genre, yearMin, yearMax, setIsLoading, navigate])
 
   return (
     <div className={`${openTrack ? "w-2/3" : "w-full"} h-full flex flex-col justify-center items-center`}>
@@ -73,20 +91,41 @@ const SearchBox : FC<{
         <input
           type="text"
           placeholder="Search..."
-          onChange={({ target }) => { handleSearch(target.value) }}
+          value={query}
+          onChange={({ target }) => { setQuery(target.value) }}
           className="mt-4 w-[500px] text-white bg-white/20 border-2 border-transparent py-2 px-3 rounded-md leading-tight outline-none placeholder:text-gray-400 focus-within:border-spotify-green transition-all duration-700" />
-        {isInvalid && <h3 className="max-w-[700px] text-center text-red-700">Invalid search query. Please use the following format: {REGEX.queryDesc}.</h3>}
+        {isInvalid && <h3 className="max-w-[750px] text-center text-red-700">Invalid search query or filters. Please use the following format: {REGEX.queryDesc}.</h3>}
+
+        <h2
+          onClick={() => setFiltersOpen(!filtersOpen)}
+          className="text-lg font-bold my-2 cursor-pointer"
+        >
+          {filtersOpen ? <MdExpandLess className="inline -mt-1" /> : <MdExpandMore className="inline -mt-1" />}
+            Advanced filters
+          {filtersOpen ? <MdExpandLess className="inline -mt-1" /> : <MdExpandMore className="inline -mt-1" />}
+        </h2>
+
+        {filtersOpen && <Filters
+          artist={artist}
+          setArtist={setArtist}
+          genre={genre}
+          setGenre={setGenre}
+          yearMin={yearMin}
+          setYearMin={setYearMin}
+          yearMax={yearMax}
+          setYearMax={setYearMax}
+        />}
       </div>
 
       {!tracks || tracks.length === 0
         ?
         <div className="relative w-full h-full mt-4 flex flex-col justify-center items-center overflow-y-auto">
-          <h1 className="mt-4 text-xl font-bold"><span className="text-spotify-green">Recommendations</span> for you, based on your favourites.</h1>
+          <h1 className="text-xl font-bold"><span className="text-spotify-green">Recommendations</span> for you, based on your favourites.</h1>
           <h1 className="text-lg italic">Start typing to search for specific tracks.</h1>
           <Recommendations customClasses="w-full h-full flex-wrap justify-center" />
         </div>
         :
-        <div className="relative w-full h-full mt-4 flex flex-wrap justify-center items-center overflow-y-auto">
+        <div className="relative w-full h-full mt-2 flex flex-wrap justify-center items-center overflow-y-auto">
           {isLoading && <Loading small={true} />}
           {tracks.map(t =>
             <TrackCard
